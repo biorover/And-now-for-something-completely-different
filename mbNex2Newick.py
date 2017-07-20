84fe1c733f7ee2626d44dc71f2c7e09e9048688e
Sean McKenzie <biorover@users.noreply.github.com>
2017-07-20 11:54:27 -0400

    Added help prompt and comments

--- mbNex2Newick.py
+++ mbNex2Newick.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
+#Sean McKenzie, graduate fellow, The Rockefeller University. License can be found in git repository (https://github.com/biorover/And-now-for-something-completely-different)
+
+#Script for taking Mr. Bayes tree output (Nexus files containing multiple newick trees with taxa symbolized with numbers and key
+# given in Nexus header) and converting it to a newick file (only newick trees with taxa names).
+help_prompt = """
+Usage: python mbNex2Newick.py MrBayesOutputTrees [burnin]
+
+
+MrBayesOutputTrees should be the Nexus file containing a series of trees, usually ending with ".run#.t"
+
+burnin is the number of trees to discard from the beginning (NOT a percentage, you can calculate that yourself
+by using 'grep -c "tree gen" fileName'
+"""
+
+
 import sys
+
+if len(sys) < 2:
+    print help_prompt
+elif sys.argv[1] in ['h','-h','help','-help','--help','--h']:
+    print help_prompt
 
 nex = open(sys.argv[1]).read().split(';')
 translate = nex[1].split('\n')[2:]
