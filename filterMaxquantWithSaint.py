1a104edbd8d0853cc3f41fa174e4837571538d72
biorover <mckensk0@gmail.com>
2020-05-21 12:16:23 -0400

    added 'subtract' and 'all' modes to filterMaxquantWithSaint.py, allowing tuning of filtering and subtraction of control intensities

--- filterMaxquantWithSaint.py
+++ filterMaxquantWithSaint.py
@@ -5,30 +5,60 @@ import sys
 
 parser = argparse.ArgumentParser(description='takes "interactions" file from \
     SAINT run (classic, not express) and uses it to remove spurious interactions\
-    from a maxquant proteinGroups file')
-parser.add_argument('--pg',help = 'maxquant proteinGroups file')
-parser.add_argument('--int', help = 'SAINT interactions file')
+    from a maxquant proteinGroups file. Control sample names (as featured in \
+    intensity column names) must all have a string not present in bait sample \
+    names (specified via the "--ctl" argument, default is "ctl")')
+required = parser.add_argument_group('required arguments')
+required.add_argument('--pg',help = 'maxquant proteinGroups file', required = True)
+required.add_argument('--int', help = 'SAINT interactions file', required = True)
 parser.add_argument('--ctl', default = 'ctl', help = 'text designation in \
     control columns. Default = "ctl"')
 parser.add_argument('--rep_sep', default = "_", help = 'separator between\
     bait name and replicate number. Set to "none" if no biological replicates. \
     Default = "_"')
+parser.add_argument('--mode', default = "bait-specific", help = '"bait-specific" \
+    or "all" (default = "bait-specific"): "bait-specific" mode sets bait-prey \
+    intensities to zero if SAINT \
+    found that specific interaction to be spurious. "all" mode drops row if \
+    SAINT found that all prey interactions were spurious.')
+parser.add_argument('--subtract', default = False, type = bool, help =
+    '"True" or "False" (default = "False"): whether to subtract ')
+parser.add_argument('--name_truncate_length', default = 99, type = int,
+    help = 'int (default = 99): length after which to truncate protein group names \
+    (added because SAINT errors on long protein names so these are usually \
+    truncated before running SAINT)')
 args = parser.parse_args()
+
+####
+#
+# Reads each line in SAINT interactions file and adds "bait<tab>prey" to
+# false_positives set if probability of true interaction < 0.95
+#
+####
 
 false_positives = set()
 
 for line in open(args.int):
     fields = line.split('\t')
-    if fields[0] != "IP" and fields[1] != "Bait":
+    if fields[0] != "IP" and fields[1] != "Bait": #in order to skip header
         if float(fields[5]) < 0.95:
             false_positives.add(fields[1] + "\t" + fields[2])
 
+####
+#
+# Reads through MaxQuant proteinGroups table and either drops false-positive
+# bait-prey interaction intensities to zero or drops rows where all interactions
+# were deemed false positives, depending on mode. Also performs control
+# subtraction if specified
+#
+####
 
 intensity_cols = []
 baits = []
+control_cols = []
 for line in open(args.pg):
     fields = line.split('\t')
-    if intensity_cols == []:
+    if intensity_cols == []: #checks if this is the header row (in which case intensity cols not yet filled in)
         for i in range(len(fields)):
             field = fields[i]
             if (field[:10] == "Intensity "):
@@ -39,13 +69,33 @@ for line in open(args.pg):
                     else:
                         baits.append(args.rep_sep.join(field[field.find(" ") + \
                             1:].split(args.rep_sep)[:-1]))
-    elif len(fields) < len(baits):
+                else:
+                    control_cols.append(i)
+    elif len(fields) < len(baits): #checks that row is not empty
         continue
-    else:
-        if len(fields[1]) > 99:
-            fields[1] = fields[1][:99] #these names are truncated in my code to convert maxquant to saint input because otherwise saint errors. This should be removed if names are not truncated in saint results
-        for i in range(len(intensity_cols)):
-            intensity_index = intensity_cols[i]
-            if baits[i] + "\t" + fields[1] in false_positives:
-                fields[intensity_index] = "0"
+    else: #this is a row with data in it!
+        if len(fields[1]) > args.name_truncate_length:
+            fields[1] = fields[1][:args.name_truncate_length]
+        if args.mode == 'bait-specific':
+            for i in range(len(intensity_cols)):
+                intensity_index = intensity_cols[i]
+                if baits[i] + "\t" + fields[1] in false_positives:
+                    fields[intensity_index] = "0"
+        elif args.mode == 'all':
+            keep_row = False #sets default to false, then we'll set it to true if any bait is missing from false positive list
+            for bait in list(set(baits)):
+                if not bait + "\t" + fields[1] in false_positives:
+                    keep_row = True
+        if not keep_row: #skips subtraction and writing output if dropping row
+            continue
+        if args.subtract: #subtracts mean control intensity from bait intensities, or sets bait intensities to zero if smaller than control
+            ctrl_mean = 0
+            for i in control_cols:
+                ctrl_mean += float(fields[i]) / len(control_cols)
+            for i in intensity_cols:
+                if int(fields[i]) > int(ctrl_mean):
+                    fields[i] = str(int(fields[i]) - int(ctrl_mean))
+                else:
+                    fields[i] = "0"
+
     sys.stdout.write("\t".join(fields))
